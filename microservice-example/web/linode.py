8a06382a1051d667dd62a1bd832ee2bce10ce82b
Ali Jabir <ali.jabir@aboutyou.de>
2021-01-21 15:05:40 +0100

    task: add v1 route to code

--- microservice-example/web/linode.py
+++ microservice-example/web/linode.py
@@ -49,6 +49,20 @@ def hello_world():
         count = (cache.get('visitor_count')).decode('utf-8')
     return 'Hello Linode!  This page has been viewed %s time(s).' % count
 
+@app.route('/v1')
+def hello_world():
+    if cache.exists('visitor_count'):
+        cache.incr('visitor_count')
+        count = (cache.get('visitor_count')).decode('utf-8')
+        update = PgFetch("UPDATE visitors set visitor_count = " + count + " where site_id = 1;", "POST")
+    else:
+        cache_refresh = PgFetch("SELECT visitor_count FROM visitors where site_id = 1;", "GET")
+        count = int(cache_refresh[0])
+        cache.set('visitor_count', count)
+        cache.incr('visitor_count')
+        count = (cache.get('visitor_count')).decode('utf-8')
+    return 'Request from route {}. This page has been viewed {} time(s).'.format('/v1',count)
+
 @app.route('/resetcounter')
 def resetcounter():
     cache.delete('visitor_count')
